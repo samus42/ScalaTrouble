6bfff656cafb9f1f336752276b30228fc69482e3
Scott MacDonald <samus42@gmail.com>
2011-11-27 16:35:30 -0600

    beginnings of command line parsing

--- console/src/main/scala/trouble/ConsoleMain.scala
+++ console/src/main/scala/trouble/ConsoleMain.scala
@@ -1,8 +1,31 @@
 package trouble
 
+import org.apache.commons.cli.{HelpFormatter, UnrecognizedOptionException, PosixParser, Options}
+
 object ConsoleMain {
   def main(args: Array[String]) {
-    println("put a game here!")
+    val options: Options = initOptions()
+    try {
+      val parser = new PosixParser();
+
+      val cmd = parser.parse(options, args);
+
+    val numCPUPlayers = cmd.getOptionValue("cpu", "1").toInt;
+    val twoColorsPerPlayer = cmd.hasOption("2c") && (numCPUPlayers == 1)
+    println("Num CPU Players: %s, 2 colors per player: %s".format(numCPUPlayers, twoColorsPerPlayer));
+    } catch {
+      case ex: UnrecognizedOptionException => {
+        println(ex.getMessage());
+        new HelpFormatter().printHelp("args", options);
+      };
+    }
+  }
+
+  private def initOptions() : Options = {
+    val options = new Options();
+    options.addOption("cpu", true, "Number of CPU players");
+    options.addOption("2c", false, "2 Colors per person");
+    options;
   }
 }
 
